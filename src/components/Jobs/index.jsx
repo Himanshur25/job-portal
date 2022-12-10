bf18b24ff180386fe51c97d7bff33eff07f78170
Himanshur25 <himanshurathi@gkmit.co>
2022-12-10 12:38:41 +0530

    fix date issue

--- src/components/Jobs/index.jsx
+++ src/components/Jobs/index.jsx
@@ -3,42 +3,55 @@ import Navbar from "../Navbar";
 import "./index.css";
 import Job from "./../../Assets/jobs.json";
 
+const relativeTime = new Intl.RelativeTimeFormat("en", {
+  style: "long",
+  numeric: "auto",
+});
+
+
 const Jobs = () => {
   return (
     <>
       <Navbar />
       <div className="job-background">
         <div className="title">
-          <h2>Jobs For You</h2>
+          <h2>Our Jobs</h2>
         </div>
       </div>
-
-      {Job.map(({ logo, company, position, location, posted, contract }) => {
-        return (
-          <div className="job-list">
-            <div className="job-card">
-              <div className="job-name">
-                <img
-                  src={require(`../../Assets/images/${logo}`)}
-                  alt="No image"
-                  className="job-profile"
-                />
-                <div className="job-detail">
-                  <h4>{company}</h4>
-                  <h3>{position}</h3>
-                  <div className="category">
-                    <p>{location}</p>
-                    <p> {posted}</p>
+      <div className="job-page">
+        {Job.map(({ logo, company, position, location, posted }) => {
+          return (
+            <div className="job-list">
+              <div className="job-card">
+                <div className="job-name">
+                  <img
+                    src={require(`../../Assets/images/${logo}`)}
+                    alt="No image"
+                    className="job-profile"
+                  />
+                  <div className="job-detail">
+                    <h4>{company}</h4>
+                    <h3>{position}</h3>
+                    <div className="category">
+                      <p>{location}</p>
+                      <p>
+                        {relativeTime.format(
+                          new Date(posted).getMonth() - new Date().getMonth(),
+                          "month"
+                        )}
+                      </p>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="job-posting">
-                <a href="">Apply Now</a>
+                <div className="job-posting">
+                  <a href="">Apply Now</a>
+                </div>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+        
+      </div>
     </>
   );
 };
