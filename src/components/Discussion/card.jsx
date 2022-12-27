be0dbe5088eabac5177a9952c61a49f79dfb53bc
Himanshur25 <himanshurathi@gkmit.co>
2022-12-27 17:19:55 +0530

    Added Discussion Page

---
+++ src/components/Discussion/card.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import { AiFillDelete } from "react-icons/ai";
+import { MdOutlineReply } from "react-icons/md";
+import parse from "html-react-parser";
+import Reply from "./reply";
+
+const Card = ({ value, onDelete }) => {
+  const { id, name, comment, rating } = value;
+  const [isOpen, setIsOpen] = useState(false);
+  const [count, setCount] = useState(0);
+  const [reply, setReply] = useState([]);
+  function addReply(name, comment) {
+    setReply([...reply, { name, comment }]);
+    setIsOpen(!isOpen);
+    setCount(count + 1);
+  }
+  return (
+    <>
+      <div className="comment-boxes" key={id}>
+        <div className="review">
+          <div className="comment">{parse(`${comment}`)}</div>
+          <div className="rating">{rating}⭐️</div>
+          <div className="name"> - {name}</div>
+
+          <div className="buttons">
+            <button className="reply" onClick={() => setIsOpen(!isOpen)}>
+              <MdOutlineReply />
+            </button>
+            <button className="delete" onClick={onDelete}>
+              <AiFillDelete />
+            </button>
+          </div>
+        </div>
+      </div>
+      {isOpen && <Reply onAdd={addReply} />}
+
+      {reply?.map(({ id, name, comment }) => {
+        return (
+          <div className="reply-section">
+            <div className="comment-boxes" key={id}>
+              <div className="review">
+                <div className="comment">{parse(`${comment}`)}</div>
+                <div className="name">
+                  <MdOutlineReply /> {name}
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default Card;
---
+++ src/components/Discussion/index.jsx
@@ -0,0 +1,107 @@
+import { useState, useRef } from "react";
+import "./index.css";
+import Navbar from "../Navbar";
+import StarRating from "./rating";
+import Card from "./card";
+
+export default function Discussion() {
+  const [comment, setComment] = useState("");
+  const [name, setName] = useState("");
+  const [count, setCount] = useState(0);
+  const [rating, setRating] = useState(0);
+  const [commentList, setCommentList] = useState([]);
+  const commentRef = useRef(null);
+  const onClickHandler = (e) => {
+    e.preventDefault();
+    setName("");
+    commentRef.current.value = "";
+    if (name === "") {
+      window.alert("Enter the credentials");
+    } else {
+      setCommentList([
+        ...commentList,
+        {
+          name,
+          comment: commentRef.current.innerHTML,
+          rating,
+          id: commentList.length + 1,
+        },
+      ]);
+      setCount(count + 1);
+    }
+  };
+  const onNameChangeHandler = (e) => {
+    setName(e.target.value);
+  };
+  const onChangeHandler = (e) => {
+    setComment(e.target.value);
+  };
+  const onDelete = (index) => {
+    let deleteList = [...commentList];
+    deleteList.splice(index, 1);
+    setCommentList(deleteList);
+    setCount(count - 1);
+  };
+  return (
+    <>
+      <Navbar />
+      <div className="discussion">
+        <div className="right">
+          <div
+            className="discuss"
+            style={{
+              textAlign: "center",
+              marginTop: "10px",
+              marginBottom: "10px",
+              fontSize: "27px",
+              fontWeight: "bold",
+            }}
+          >
+            MARK YOUR REVIEWS
+          </div>
+
+          <form className="comment-form">
+            <div className="input-group">
+              <span className="input-group-add">Name</span>
+              <input
+                type="text"
+                placeholder="Your name"
+                className="form-control"
+                value={name}
+                onChange={onNameChangeHandler}
+              />
+            </div>
+            <div className="input-group">
+              <span className="input-group-add">Comment</span>
+              <div
+                type="text"
+                className="form-control comment"
+                contentEditable="true"
+                data-placeholder="Enter your review"
+                value={comment}
+                onChange={onChangeHandler}
+                ref={commentRef}
+              />
+            </div>
+            <div>
+              Your Rating :-
+              <StarRating onChange={setRating} />
+            </div>
+
+            <button className="submit" onClick={onClickHandler}>
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+      <div className="line">Comments ({count})</div>
+      <div className="comment-section">
+        <div className="comment-box">
+          {commentList.map((data) => {
+            return <Card key={data.id} value={data} onDelete={onDelete} />;
+          })}
+        </div>
+      </div>
+    </>
+  );
+}
